f02f38ee973465514d25f3e617c84ecc1e03c52e
pyrysj <pyry.sjovall@outlook.com>
2021-11-27 23:20:27 +0200

    basic api interactions, but nothing concrete

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
-print("hello world!")
+import json
+import asyncio
+import websockets
+import requests
+
+with open ('config.json') as config:
+    data = json.load(config)
+
+identity = {
+    "op": 2,
+    "d":{
+        "token": data['token'],
+        "intents": 513,
+        "properties": {
+            "$os": "mac_os",
+            "$browser": "my_library",
+            "$device": "my_library"
+        }
+    }
+}
+
+async def main():
+    r = requests.get('https://discord.com/api/v9/gateway/bot', headers={"Authorization":f"Bot {data['token']}"})
+    answer = r.json()
+
+    async with websockets.connect(answer["url"]) as websocket:
+        message = await websocket.recv()
+        print(message)
+        await websocket.send(json.dumps(identity))
+        message = await websocket.recv()
+        print(message)
+
+asyncio.run(main())
